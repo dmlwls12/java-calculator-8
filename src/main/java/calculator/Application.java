4711c777349d5ec3cd047d1d56dcfc88d17f010b
dmlwls12 <gimuijin572@gmail.com>
2025-10-20 10:48:28 +0900

    feat(calculator): implement addition using comma and colon delimiters
    - 문자열을 쉼표와 콜론으로 분리해 각 숫자의 합을 계산한다.
    - 빈 문자열 또는 공백 입력은 0을 반환하도록 한다.
    - 결과는 "결과:{값}" 형식으로 출력한다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,28 @@
 package calculator;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해주세요.");
+        String input = Console.readLine();
+        int result = add(input);
+        System.out.println("결과 : " + result);
+
+        Console.close();
+    }
+
+    public static int add(String input){
+
+        if(input == null || input.isBlank()) return 0;
+
+        String[] arr = input.split("[,:]");
+        int sum = 0;
+        for(String s : arr){
+            if(!s.isBlank()){
+                sum += Integer.parseInt(s.trim());
+            }
+        }
+        return sum;
     }
 }
